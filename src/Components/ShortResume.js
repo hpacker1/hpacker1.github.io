b5c88694d03a9502b821e12ccf90917379510959
Jo-Ann Packer <jo-annpacker@jo-anns-mbp.myfiosgateway.com>
2020-11-15 19:41:26 -0500

    last picture added, image styling and titles changed

--- src/Components/ShortResume.js
+++ src/Components/ShortResume.js
@@ -4,6 +4,7 @@ import ibmlogo from "../images/ibmresearchlogo.jpg";
 import mitlogo from "../images/mitlllogo.jpg";
 import kvhlogo from "../images/kvhlogo.jpg";
 import optumlogo from "../images/optumlogo.jpg";
+import beaverworkslogo from "../images/beaverworkslogo.png";
 
 class ShortResume extends Component {
   render() {
@@ -15,34 +16,66 @@ class ShortResume extends Component {
             <h3>Here are some places I've worked.</h3>
 
             <img className="resume-images" src={ibmlogo} alt="ibm" />
-            <p style={{ width: "50%", margin: "auto" }}>
-              I'm currently working as a Full Stack Software Engineer at IBM
-              Research on internal project management tools.
+            <div className="subTitle">IBM Research</div>
+            <div className="date">Aug '20 - Present</div>
+            <p style={{ width: "40%", margin: "auto" }}>
+              At IBM Research, I develop tools that help researchers manage
+              their projects and collaborate.
             </p>
+            <br />
+            <br />
+            <br />
             <Grid>
               <Cell col={6}>
-                <img className="resume-images" src={mitlogo} alt="ibm" />
-                <p style={{ width: "50%", margin: "auto" }}>
-                  I'm currently working as a Full Stack Software Engineer at IBM
-                  Research on internal project management tools.
+                <img className="resume-images-light" src={mitlogo} alt="mit" />
+                <div className="subTitle">MIT Lincoln Laboratory</div>
+                <div className="date">Mar '19 - Aug '20</div>
+                <p>
+                  I helped create GIS applications for organizing and displaying
+                  large amounts of geographic data from a browser.
                 </p>
               </Cell>
               <Cell col={6}>
-                <img className="resume-images" src={kvhlogo} alt="ibm" />
-                <p>I interned at Optum for one summer</p>
+                <img className="resume-images" src={kvhlogo} alt="kvh" />
+                <div className="subTitle">KVH</div>
+                <div className="date">Summer '18</div>
+                <p>
+                  As an intern at KVH, I worked on the odomotry for an early
+                  autonomous aquatic vehicle project using the NVIDIA Drive PX2.
+                  I also worked on the video player and UI for a streaming
+                  service targeted for ship crews.
+                </p>
               </Cell>
             </Grid>
+            <br />
+            <br />
+            <br />
             <Grid>
               <Cell col={6}>
-                <img className="resume-images" src={optumlogo} alt="ibm" />
-                <p style={{ width: "50%", margin: "auto" }}>
+                <img
+                  className="resume-images-light"
+                  src={optumlogo}
+                  alt="optum"
+                />
+                <div className="subTitle">Optum</div>
+                <div className="date">Summer '17</div>
+                <p>
                   I'm currently working as a Full Stack Software Engineer at IBM
                   Research on internal project management tools.
                 </p>
               </Cell>
               <Cell col={6}>
-                <img className="resume-images" src={kvhlogo} alt="ibm" />
-                <p>I interned at Optum for one summer</p>
+                <img
+                  className="resume-images-light"
+                  src={beaverworkslogo}
+                  alt="beaverworks"
+                />
+                <div className="subTitle">MIT Beaver Works</div>
+                <div className="date">Mar '19 - Aug '20</div>
+                <p>
+                  Through collaboration with MIT, I helped teach kids about
+                  beginner programming principles and robotics.
+                </p>
               </Cell>
             </Grid>
 
