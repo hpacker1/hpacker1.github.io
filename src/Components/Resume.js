079b92986dba197dae7c505da91a843095c6061b
Jo-Ann Packer <jo-annpacker@Jo-Anns-MBP.fios-router.home>
2021-03-09 17:51:54 -0500

    game description and url changed

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -7,9 +7,9 @@ class Resume extends Component {
             <div>
                 <h1>Projects</h1>
                 <p>
-                    This is a test project, but stay tuned more projects coming soon!
+                    This is a short game about analog robots that react to light.
                 </p>
-                <iframe title="test1" src="https://gotm.io/hpacker1/testgame?forceFullscreen=true" width="1000px" height="1000px"></iframe>
+                <iframe title="test1" src="https://gotm.io/hpacker1/clevercars?forceFullscreen=true" width="1000px" height="1000px"></iframe>
 
 
                 
